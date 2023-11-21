67e4feac74413244945206dfdedadc53740b3ca6
sullaspqr <sulla.spqr78@gmail.com>
2023-11-21 20:03:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pizzas, setPizzas] = useState([]);
+  const [isFetchPending, setFetchPending] = useState(false);
+
+  useEffect(() => {
+      setFetchPending(true);
+      fetch("https://pizza.kando-dev.eu/Pizza",)
+      .then((res) => res.json())
+      .then((pizzak) => setPizzas(pizzak))
+      .catch(console.log)
+      .finally(() => {
+          setFetchPending(false);
+      });
+  }, []);
+
+  return(
+      <div className="p-5 m-auto text-center content bg-ivory">
+          {isFetchPending ? (
+              <div className="spinner-border"></div>) :
+              (<div>
+                  <h2>Pizzák</h2>
+                  {pizzas.map((pizza) => (
+                      <div className="card col-sm-3 d-inline-block m-1 p-2" key={pizza.id}>
+                          <h6 className="text-muted">Pizza neve: {pizza.name}</h6>
+                          <h5 className="text-dark">Gluténmentes-e: {pizza.isGlutenFree === 1 ? 'Igen': 'Nem'}</h5>
+                          <div className="card-body">
+                              <img
+                              className="img-fluid"
+                              style={{ maxHeight: 200 }}
+                              src={pizza.kepURL ? pizza.kepURL : "https://via.placeholder.com/400x800"}
+                              alt="pizza-helyettesito"/>
+                       </div>
+                      </div>  
+                  ))}
+              </div>
+          )}
+      </div>
+  )
+  ;
 }
 
 export default App;
